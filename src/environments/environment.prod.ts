80e89a217b20c27ef46b1659d03df94d60a50116
nichitacebotari0 <nichitacebotari0@gmail.com>
2022-12-11 00:31:13 +0000

    set api path to api.fangsbuilder.com

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseUrl: "https://20.31.199.237/api/",
+  apiBaseUrl: "https://api.fangsbuilder.com/api/",
   authUrl: "https://discord.com/api/oauth2/authorize?client_id=1049648726664282183&redirect_uri=https%3A%2F%2Fwww.fangsbuilder.com%2Fdiscord-redirect&response_type=code&scope=identify%20guilds.members.read"
 };
